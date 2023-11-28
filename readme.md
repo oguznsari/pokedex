3ea04b5aa2088d753310dc4f0f5940b6cca1d5c9
Oğuzhan SARI <oguzn.sari@gmail.com>
2023-11-28 23:07:55 +0300

    Refactor: folder structure + redux

---
+++ readme.md
@@ -0,0 +1,16 @@
+```bash
+# to start the application
+yarn ios
+```
+
+```bash
+# packages
+yarn add react-redux @reduxjs/toolkit
+```
+
+<pre>
+src
+├── app             -- redux params & store   
+├── components      -- components
+└── features        -- feature bazlı
+</pre>
---
+++ src/app/services/pokemonApi.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseUrl = "https://my-json-server.typicode.com/oguznsari/pokedex";
+
+export const pokemonApi = createApi({
+  reducerPath: "pokemonApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl,
+  }),
+  endpoints: (builder) => ({
+    getPokemons: builder.query({
+      query: () => "/pokemons",
+    }),
+  }),
+});
+
+export const { useGetPokemonsQuery } = pokemonApi;
